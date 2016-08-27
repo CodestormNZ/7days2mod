059ae68b06d02cbab3ff8117d5e30144ad19110b
StompyNZ <russell@codestorm.nz>
2016-08-27 21:59:53 +1200

    PSR-2 formatting

--- 7days2mod/code/extensions/modSiteConfig.php
+++ 7days2mod/code/extensions/modSiteConfig.php
@@ -1,18 +1,21 @@
 <?php
 
-class modSiteConfig extends DataExtension {
-
+class modSiteConfig extends DataExtension
+{
     private static $db = array(
         'gituser' => 'Text',
         'gitpwd' => 'Text'
     );
 
-    public function updateCMSFields(FieldList $fields) {
-        $fields->addFieldToTab("Root.Main", 
-            new TextField("gituser", "GitHub Username or Email")
+    public function updateCMSFields(FieldList $fields)
+    {
+        $fields->addFieldToTab(
+          "Root.Main", 
+          new TextField("gituser", "GitHub Username or Email")
         );
-        $fields->addFieldToTab("Root.Main", 
-            new PasswordField("gitpwd", "GitHub Password")
+        $fields->addFieldToTab(
+          "Root.Main", 
+          new PasswordField("gitpwd", "GitHub Password")
         );
     }
 }
