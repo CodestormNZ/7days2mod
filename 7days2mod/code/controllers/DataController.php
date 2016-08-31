61cfe13d2e4c931177cf008ce3b3fcfc7c78ab16
StompyNZ <russell@codestorm.nz>
2016-08-31 20:58:15 +1200

    Added Compare View

--- 7days2mod/code/controllers/DataController.php
+++ 7days2mod/code/controllers/DataController.php
@@ -6,6 +6,7 @@ class DataController extends Controller
     'showGUI',
     'showCodemirror',
     'showNode',
+    'showCompare',
   );
   
   private static $url_handlers = array(
@@ -15,6 +16,10 @@ class DataController extends Controller
     'Config/$file/code' => 'showCodemirror',
     'Config/$folder/$file/node' => 'showNode',
     'Config/$file/node' => 'showNode',
+    'Config/$folder/$file/compare/$org2/$repo2/code' => 'showCompare',
+    'Config/$file/compare/$org2/$repo2/code' => 'showCompare',
+    'Config/$folder/$file/compare/$repo2/code' => 'showCompare',
+    'Config/$file/compare/$repo2/code' => 'showCompare',
 //    'Config/$folder/!$file' => 'showGUI', //does not match on Config/XUi/filename.xml !!!!!!!!!
 //    'Config/$file' => 'showGUI',
   );
@@ -34,6 +39,8 @@ class DataController extends Controller
     $params = new stdClass();
     $params->org = '7days2mod';
     $params->repo = 'Vanilla';
+    $params->org2 = '7days2mod';
+    $params->repo2 = 'Vanilla';
     $params->folder = 'Data/Config/';
     $params->extension = '';
     if (null !== $this->getRequest()->param('org')) {
@@ -41,6 +48,12 @@ class DataController extends Controller
     }
     if (null !== $this->getRequest()->param('repo')) {
       $params->repo = $this->getRequest()->param('repo');
+    }
+    if (null !== $this->getRequest()->param('org2')) {
+      $params->org2 = $this->getRequest()->param('org2');
+    }
+    if (null !== $this->getRequest()->param('repo2')) {
+      $params->repo2 = $this->getRequest()->param('repo2');
     }
     if (null !== $this->getRequest()->param('folder')) {
       $params->folder = $params->folder.$this->getRequest()->param('folder')."/";
@@ -132,7 +145,6 @@ CSS;
     }
   }
 
-
   public function showNode()
   {
     $content = '';
@@ -180,6 +192,109 @@ CSS;
     return $this->customise(new ArrayData(array(
       'uniqueNodes' => $content,
     )))->renderWith("RepoData_node");
+  }
+  
+  public function showCompare()
+  {
+    $params = $this->loadConfigParams();
+    $responseData = $this->getGitHubContent($params->file_path, $params->org, $params->repo);
+    $responseData2 = $this->getGitHubContent($params->file_path, $params->org2, $params->repo2);
+    if (isset($responseData->content)) {
+      $content = str_replace('"', '\"', base64_decode($responseData->content));
+      $content = str_replace("\n", '\n', $content);
+    } else {
+      $content = "File not found in repo: ".$params->file_path;
+    }
+    if (isset($responseData2->content)) {
+      $content2 = str_replace('"', '\"', base64_decode($responseData2->content));
+      $content2 = str_replace("\n", '\n', $content2);
+    } else {
+      $content2 = "File not found in repo2: ".$params->file_path;
+    }
+    
+    Requirements::javascript("//codemirror.net/lib/codemirror.js");
+    Requirements::css("//codemirror.net/lib/codemirror.css");
+    Requirements::javascript("//codemirror.net/mode/xml/xml.js");    
+    Requirements::javascript("//cdnjs.cloudflare.com/ajax/libs/diff_match_patch/20121119/diff_match_patch.js");
+    Requirements::javascript("//codemirror.net/addon/merge/merge.js");
+    Requirements::css("//codemirror.net/addon/merge/merge.css");
+
+    $js = <<<JS
+      var repo1, repo2, dv, panes = 2, highlight = true, connect = null, collapse = false;
+      function initUI() {
+        if (repo1 == null) return;
+        var target = document.getElementById("xmlcode");
+        target.innerHTML = "";
+        dv = CodeMirror.MergeView(target, {
+          value: repo1,
+          orig: repo2,
+          lineNumbers: true,
+          highlightDifferences: highlight,
+          connect: connect,
+          collapseIdentical: collapse
+        });
+      }
+
+      function toggleDifferences() {
+        dv.setShowDifferences(highlight = !highlight);
+      }
+
+      window.onload = function() {
+        repo1 = "$content";
+        repo2 = "$content2";
+        initUI();
+      };
+
+      function mergeViewHeight(mergeView) {
+        function editorHeight(editor) {
+          if (!editor) return 0;
+          return editor.getScrollInfo().height;
+        }
+        return Math.max(editorHeight(mergeView.leftOriginal()),
+                        editorHeight(mergeView.editor()),
+                        editorHeight(mergeView.rightOriginal()));
+      }
+
+      function resize(mergeView) {
+        var height = mergeViewHeight(mergeView);
+        for(;;) {
+          if (mergeView.leftOriginal())
+            mergeView.leftOriginal().setSize(null, height);
+          mergeView.editor().setSize(null, height);
+          if (mergeView.rightOriginal())
+            mergeView.rightOriginal().setSize(null, height);
+
+          var newHeight = mergeViewHeight(mergeView);
+          if (newHeight >= height) break;
+          else height = newHeight;
+        }
+        mergeView.wrap.style.height = height + "px";
+      }
+JS;
+    Requirements::customScript($js);
+
+    $css = <<<CSS
+      .CodeMirror { line-height: 1.2;}
+      .CodeMirror-merge, .CodeMirror-merge .CodeMirror {
+        height: 650px;
+      }
+      @media screen and (min-width: 1300px) {
+        article { max-width: 100%; }
+        #nav { border-right: 499px solid transparent; }
+      }
+      span.clicky {
+        cursor: pointer;
+        background: #d70;
+        color: white;
+        padding: 0 3px;
+        border-radius: 3px;
+      }
+CSS;
+    Requirements::customCSS($css);
+    
+    return $this->customise(new ArrayData(array(
+      'Content' => "",
+    )))->renderWith("RepoData_compare");
   }
 
 }
